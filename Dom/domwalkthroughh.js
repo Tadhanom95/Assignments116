879f78da1f5fb0917fb43c078d79efbebeaec486
Tadesse Adhanom <Tadhanom95@gmail.com>
2022-05-24 14:09:33 -0700

    domwalkthroughh

---
+++ Dom/domwalkthroughh.js
@@ -0,0 +1,7 @@
+//console.dir(document);
+
+console.log(document.domain);//<-- shows IP adress-->
+console.log(document.URL);//Shows The URL on console
+console.log(document.title);//It going to show us the title of the document
+console.log(document.doctype);//shows the doctype of the document
+console.log(document.head);//showing head of the document on the console
