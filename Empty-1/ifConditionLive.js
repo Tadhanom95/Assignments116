40393b8902f94e6ccde00cb89b68200ba59c2073
Tadesse Adhanom <Tadhanom95@gmail.com>
2022-06-07 17:25:21 -0700

    if condition

---
+++ Empty-1/ifConditionLive.js
@@ -0,0 +1,41 @@
+ //
+ console.log("About to code Tade!");
+//Defining the logic to get any math random number
+ let randomNumber = Math.random();
+
+ console.log(randomNumber);
+// //
+//  if (randomNumber < 0.5) {
+//      console.log("we coding");
+//      console.log(randomNumber);
+//  }
+//  //
+//  if (randomNumber >= 0.5) {
+//      console.log('Random Number is greater than 0.5');
+//      console.log(randomNumber);
+//  }
+//  If Else Statement:
+  // giving loging for the var
+// const dayOfWeek = 'Thursday';
+// // if the all the condition meet we will get the value we gave
+// if (dayOfWeek==='Monday'){
+//     console.log('This is Monday');
+// // if the condtion did not meet or failed we will get the Thursday value
+// }else if (dayOfWeek==='Thursday'){
+//     console.log("This is Thursday");
+// // same if the logic failed we will get saturday value
+// } else if (dayOfWeek==='Saturday') {
+//     console.log('Hooray,today is Saturday');
+// }
+// // Creating a variable for age to enter an age
+// const age = prompt ('Enter An Age');
+// //const age = 8;
+// // setting logic if the condition meets the expectation
+//  if (age < 5) {
+//      console.log("You are less than 5! Cmon bro.");
+// // creating variable if the condition logic failed to meet then say this value
+//  }else if (age < 10) {
+//      console.log("still too young!");
+//  } else if (age >- 16){
+//      console.assertlog("You are able to drive");
+//  }
